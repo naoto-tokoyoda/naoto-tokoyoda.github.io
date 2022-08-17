aaebbaae82d0ee3f289c1914c51e25aea4485bf3
Naoto Tokoyoda <tkyd89@gmail.com>
2022-08-17 01:28:01 -0700

    updated

--- src/components/pages/projects/Projects.js
+++ src/components/pages/projects/Projects.js
@@ -25,6 +25,9 @@ function Projects() {
             <ProjectsUI />
           </Container>
         : <ProjectsUI />}
+        {/* <Container className={classes.projects_container} >
+            <ProjectsUI />
+          </Container> */}
         
     </div>
   )
