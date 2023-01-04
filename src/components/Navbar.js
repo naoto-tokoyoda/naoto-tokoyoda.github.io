00700ba60d7d4112da76695bc2cef45b60d3b5b2
Naoto Tokoyoda <tkyd89@gmail.com>
2023-01-03 16:28:39 -0800

    updated

--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -6,7 +6,7 @@ function Navbar() {
   return (
     <nav className={classes.nav}>
         <div className={classes.title}>
-            <h1><a href="Home">Naoto's portfolio</a></h1>
+            <h1><a href="#home">Naoto's portfolio</a></h1>
         </div>
         <ul className={classes.links}>
             <li><a href="#home">Home</a></li>
